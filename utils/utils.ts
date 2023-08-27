fd408334589d808be0e61d4527b8559d0dfd7ac3
highDream0828 <softwaredev0828@gmail.com>
2023-08-28 01:14:59 +0900

    new

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_IMAGE_URL: string = "https://imgur.com/5dYYce8.png";
-export const AvatarImage: string = "/avatar/avatar.jpg";
-export const homeProfileImage: string = "/avatar/avatar.jpg";
+export const AvatarImage: string = "/avatar/avatar.png";
+export const homeProfileImage: string = "/avatar/avatar.png";
 
 export const navigationRoutes: string[] = [
   "home",
